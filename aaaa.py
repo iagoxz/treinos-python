7d24c0cc8d4581786af0687e74309ae487dd6eff
iagoxz <99759487+iagoxz@users.noreply.github.com>
2022-05-31 23:21:03 -0300

    Add files via upload

---
+++ aaaa.py
@@ -0,0 +1,4 @@
+d = int(input('quantos dias o carro foi alugado: '))
+km = int(input('qantos km rodados: '))
+preço = 60 * d + 0,15 * km
+print('o valor e:{}'.format (preço))
---
+++ pratice.py
@@ -0,0 +1,4 @@
+larg = float(input('qual largura da parede: '))
+alt = float(input('qual altura da parede: '))
+area = larg * alt
+print('a area e de: ', area)
